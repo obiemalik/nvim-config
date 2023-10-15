5117b0c917a5e70045cbc0548613b625272eb3f6
Obie Malik <obiemalik@gmail.com>
2023-10-14 20:09:04 -0400

    Update 'indent-blankline' setup configuration (#1)
    Co-authored-by: Obie Malik <obiemalik@gmail.com>

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -3,18 +3,44 @@
 -----------------------------------------------------------
 -- Plugin: indent-blankline
 -- url: https://github.com/lukas-reineke/indent-blankline.nvim
-local status_ok, indent_blankline = pcall(require, 'indent_blankline')
+local status_ok, indent_blankline = pcall(require, 'ibl')
 if not status_ok then
   return
 end
 
+---
+
+local highlight = {
+  "RainbowRed",
+  "RainbowYellow",
+  "RainbowBlue",
+  "RainbowOrange",
+  "RainbowGreen",
+  "RainbowViolet",
+  "RainbowCyan",
+}
+
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
 indent_blankline.setup {
-  char = "▏",
-  use_treesitter = true,
-  show_first_indent_level = false,
-  filetype_exclude = {
-    'help', 'dashboard', 'git', 'markdown', 'text', 'terminal', 'lspinfo',
-    'packer', 'NvimTree',
+  indent = { highlight = highlight },
+  scope = {
+    exclude = {
+      language = {
+        'help', 'dashboard', 'git', 'markdown', 'text', 'terminal', 'lspinfo',
+        'packer', 'NvimTree',
+      }
+    }
   },
-  buftype_exclude = {'terminal', 'nofile'},
 }
